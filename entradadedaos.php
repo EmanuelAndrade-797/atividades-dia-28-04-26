80a978e84ec9897cfdd5bb634b1143f8bdf78781
EmanuelAndrade-797 <emanuel.andrade@edu.senai.br>
2026-04-28 10:22:20 -0300

    v.0.2 geometria e conversao
    agora tem o dolar e a area do quadrado

--- entradadedaos.php
+++ entradadedaos.php
@@ -1,5 +1,10 @@
 <?php
 require_once "biblioteca_de_funcoes.php";
 use function conversao\dolarparareal;
+use function geometria\areaQuadrado;
 echo "em dolar o valor e:  ", dolarparareal (100, 5);
+echo "a area do quadrade e: ", areaQuadrado (20);
+
+
+
 ?>
