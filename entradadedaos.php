db2ff307dd14979771ef6f7456e5d89a36391e61
EmanuelAndrade-797 <emanuel.andrade@edu.senai.br>
2026-04-28 11:24:14 -0300

    v.0.03
    calculo da conversao de dolar.imc e area do quadrado

--- entradadedaos.php
+++ entradadedaos.php
@@ -2,9 +2,10 @@
 require_once "biblioteca_de_funcoes.php";
 use function conversao\dolarparareal;
 use function geometria\areaQuadrado;
+use function saude\calcularimc;
 echo "em dolar o valor e:  ", dolarparareal (100, 5);
 echo "a area do quadrade e: ", areaQuadrado (20);
-
+echo "o seu IMC é: ", calcularimc (175, 60);
 
 
 ?>
